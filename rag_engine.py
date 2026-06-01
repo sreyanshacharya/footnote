df22274b428ba51b6da8802a286ccb04ff63af34
Sreyansh Acharya <6inthesky@gmail.com>
2026-06-01 17:26:21 +0530

    modified for fastapi integration

--- rag_engine.py
+++ rag_engine.py
@@ -4,38 +4,46 @@ import pickle
 import torch
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
 
 #load embedding model
 emb_model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 #load index and chunks
-index_path = "model-files/index.faiss"
-chunks_path = "model-files/chunks.pkl"
+index_path = "model_files/index.faiss"
+chunks_path = "model_files/chunks.pkl"
 index_exists = False
 chunks_exists = False
 if Path(index_path).is_file():
-  index = faiss.read_index("model-files/index.faiss")
   index_exists = True
 
 if Path(chunks_path).is_file():
-    with open("model-files/chunks.pkl", "rb") as f:
-      chunks = pickle.load(f)
     chunks_exists = True
 
 #load llm and tokenizer
+quantization_config = BitsAndBytesConfig(
+  load_in_4bit=True,
+  bnb_4bit_quant_type="nf4",
+  bnb_4bit_use_double_quant=True,
+  bnb_4bit_compute_dtype=torch.bfloat16
+)
+
 model_name = "microsoft/Phi-4-mini-instruct"
 llm = AutoModelForCausalLM.from_pretrained(
   model_name,
-  dtype=torch.float16,
+  quantization_config=quantization_config,
   device_map='auto'
 )
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 #query retrieval
 def retrieve(question):
-  if(index_exists and chunks_exists):
+  if(Path(index_path).is_file() and Path(chunks_path).is_file()):
+    index = faiss.read_index("model_files/index.faiss")
+    with open("model_files/chunks.pkl", "rb") as f:
+      chunks = pickle.load(f)
+
     qvec = emb_model.encode([question]).astype("float32")
     faiss.normalize_L2(qvec)
     #index search
@@ -43,7 +51,13 @@ def retrieve(question):
     context = "\n\n".join([chunks[idx] for idx in I[0]])
     return context
   else:
-    raise RuntimeError()
+    import os
+    print(f"\n--- debug engine room ---")
+    print(f"current working directory: {os.getcwd()}")
+    print(f"looking for index at: {Path(index_path).absolute()} | exists: {Path(index_path).exists()}")
+    print(f"looking for chunks at: {Path(chunks_path).absolute()} | exists: {Path(chunks_path).exists()}")
+    print(f"-------------------------\n")
+    raise RuntimeError("missing faiss index or chunks files on disk.")
   
 #answer generation
 def generate(context, question):
