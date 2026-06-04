e7273b2ad0ae6f1dd7b23c81c0a0dc0a8948f9e9
Sreyansh Acharya <6inthesky@gmail.com>
2026-06-04 15:13:30 +0530

    linked streamlit ui with fastapi backend

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
-from rag_engine import ask
-from ingest import ingest
+import requests
 from pathlib import Path
 
 st.set_page_config(page_title='Footnote', page_icon='📟')
 st.title("📟 Footnote")
 st.caption("Locally Run RAG-based Study Assistant")
+
+request_url  = "http://127.0.0.1:8000"
 
 #Upload area
 st.subheader("Upload Notes (copyable pdfs only) : ")
@@ -16,28 +17,42 @@ uploaded_files = st.file_uploader(
 )
 
 if uploaded_files:
-  Path("data").mkdir(exist_ok=True)
-
   for file in uploaded_files:
-    with open(Path("data") / file.name, "wb") as f:
-      f.write(file.getbuffer())
-
-  if st.button("Ingest uploaded files"):
-    with st.spinner("Adding uploade pdfs into my mind"):
-      pdfscount, chunkscount = ingest()
-
-    st.success(f"Ingested {pdfscount} PDFs into {chunkscount} chunks !")
-    st.rerun()
+    try:
+      file_bytes = file.getvalue()
+      files = {
+          "file": (file.name, file_bytes, "application/pdf")
+      }
+      response = requests.post(f"{request_url}/ingest", files=files)
+      
+      if response.status_code == 200:
+          result = response.json()
+          st.success(f"successfully loaded! index now contains {result['ingested no. of pdfs']} files.")
+      else:
+          st.error("failed to process document on backend server.")
+            
+    except requests.exceptions.ConnectionError:
+        st.error("backend server is offline.")
 
 #Query area
-user_question = st.text_input("Ask Your Notes :")
+user_question = st.chat_input("Ask Your Notes :")
 
 if user_question:
   with st.spinner("Turning my gears..."):
-    answer, context = ask(user_question)
-  
-  st.markdown('### Answer')
-  st.write(answer)
+    try:
+      payload = {"question" : user_question}
+      response = requests.post(f"{request_url}/ask", json=payload)
 
-  with st.expander("context used :"):
-    st.write(context)
+      if response.status_code == 200:
+        data = response.json()
+        answer = data["answer"]
+        context = data["context"]
+        st.markdown('### Answer')
+        st.write(answer)
+        with st.expander("context used :"):
+          st.write(context)
+      else :
+        st.error(f"backend error with response code : {response.status_code}")
+
+    except requests.exceptions.ConnectionError:
+      st.error("Could not connect to FastAPI.")
